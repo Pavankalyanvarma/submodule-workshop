8117fcf61a94d98cb7e08977f25c04482206bdf0
MudUnuru Pavan Kalyan Varma <pavanvarma@leewayhertz.com>
2023-10-18 17:22:51 +0530

    2ND commit

---
+++ sub.js
@@ -0,0 +1,13 @@
+function getArray() {
+    return [1,2,3,4,5,6,7,8];
+    
+  }
+  function AlphaArray(){
+    return [a,b,c];
+
+  }
+  const myArray = getArray();
+  const myAlphaArray= AlphaArray();
+  console.log(myArray);
+  console.log(myAlphaArray);
+  
